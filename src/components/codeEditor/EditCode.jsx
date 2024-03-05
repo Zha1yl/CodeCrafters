08cf592a563c29eaba8a063ed4c7dad594eecb2f
EsenZhanyshov <e.zhanyshov@gmail.com>
2024-03-05 10:51:49 +0600

    conflicts

--- src/components/codeEditor/EditCode.jsx
+++ src/components/codeEditor/EditCode.jsx
@@ -1,9 +1,9 @@
 import { Box, HStack } from "@chakra-ui/react";
-import { Editor } from "@monaco-editor/react";
 import React, { useRef, useState } from "react";
 import CodeLanguageSelector from "./CodeLanguageSelector";
 import { CODE_SNIPPETS } from "../../helpers/const";
 import OutputCode from "./OutputCode";
+import { Editor } from "@monaco-editor/react";
 
 const EditCode = () => {
   const editorRef = useRef();
@@ -19,12 +19,11 @@ const EditCode = () => {
   };
   return (
     <Box>
-      <HStack spacing={4}>
+      <div>
         <Box width="50%">
           <CodeLanguageSelector language={language} onSelect={onSelect} />
           <Editor
             height="75vh"
-            theme="vs-dark"
             language={language}
             defaultValue={CODE_SNIPPETS[language]}
             onMount={onMount}
@@ -33,7 +32,7 @@ const EditCode = () => {
           />
         </Box>
         <OutputCode editorRef={editorRef} language={language} />
-      </HStack>
+      </div>
     </Box>
   );
 };
