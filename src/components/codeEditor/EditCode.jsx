ee4c6e9f4029d6d003b6c5a42d6a447cf80164d3
wunknown2 <wunknown2@gmail.com>
2024-03-05 10:08:28 +0300

    code editor

---
+++ src/components/codeEditor/EditCode.jsx
@@ -0,0 +1,41 @@
+import { Box, HStack } from "@chakra-ui/react";
+import { Editor } from "@monaco-editor/react";
+import React, { useRef, useState } from "react";
+import CodeLanguageSelector from "./CodeLanguageSelector";
+import { CODE_SNIPPETS } from "../../helpers/const";
+import OutputCode from "./OutputCode";
+
+const EditCode = () => {
+  const editorRef = useRef();
+  const [value, setValue] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const onMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
+  const onSelect = (language) => {
+    setLanguage(language);
+    setValue(CODE_SNIPPETS[language]);
+  };
+  return (
+    <Box>
+      <HStack spacing={4}>
+        <Box width="50%">
+          <CodeLanguageSelector language={language} onSelect={onSelect} />
+          <Editor
+            height="75vh"
+            theme="vs-dark"
+            language={language}
+            defaultValue={CODE_SNIPPETS[language]}
+            onMount={onMount}
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+        </Box>
+        <OutputCode editorRef={editorRef} language={language} />
+      </HStack>
+    </Box>
+  );
+};
+
+export default EditCode;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,10 @@
-
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import Auth from "../pages/Auth";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
-
+import CodeEditorPage from "../pages/CodeEditorPage";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
@@ -13,6 +12,7 @@ const MainRoutes = () => {
     { id: 2, link: "/auth", element: <Auth /> },
     { id: 3, link: "/login", element: <Login /> },
     { id: 4, link: "/register", element: <Register /> },
+    { id: 5, link: "/codeedit", element: <CodeEditorPage /> },
   ];
   return (
     <div>
