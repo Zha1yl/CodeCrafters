ece0e394fc6d36734707e58b1795d9570469e6d2
wunknown2 <wunknown2@gmail.com>
2024-03-05 10:49:49 +0300

    theme

--- src/pages/CodeEditorPage.jsx
+++ src/pages/CodeEditorPage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import EditCode from "../components/codeEditor/EditCode";
-import { Box } from "@chakra-ui/react";
 
 const CodeEditorPage = () => {
   return (
-    <Box minH="100vh" px={6} py={8}>
+    <div style={{ minHeight: "95vh", padding: "32px, 24px" }}>
       <EditCode />
-    </Box>
+    </div>
   );
 };
 
